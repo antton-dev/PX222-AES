62c5c2a0b8603b5f10ac13d8d1d1eaca49309eb7
antton-dev <antton.chevrier@etik.com>
2026-06-08 15:45:19 +0200

    test cipher ok

--- C/fichier_test.c
+++ C/fichier_test.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include "Aes.h"
+#include "cipher.h"
 
 bool compare(uint8_t state1[4][4], uint8_t state2[4][4]) {
     for (int i = 0; i < 4; i++) {
@@ -11,6 +12,16 @@ bool compare(uint8_t state1[4][4], uint8_t state2[4][4]) {
         }
     }
     return true;
+}
+
+void transpose(uint8_t State[4][4]) {
+    uint8_t temp[4][4]; 
+    memcpy(temp, State, 16*sizeof(uint8_t));
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            State[i][j] = temp[j][i];
+        }
+    }
 }
 
 int main() {
@@ -215,6 +226,36 @@ int main() {
     } else {
         printf("False\n\n");
     }
+
+
+    // ---------------------------------------------------------
+    //          TEST 8 : Chiffrement AES-128 
+    //          D'après l'annexe B de la documentation
+    // ---------------------------------------------------------
+
+
+    uint8_t key[16] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
+
+    uint8_t input[4][4] = {{0x32, 0x88, 0x31, 0xe0},{ 0x43, 0x5a, 0x31, 0x37}, {0xf6, 0x30, 0x98, 0x07}, {0xa8, 0x8d, 0xa2, 0x34}};
+
+    uint8_t AES128_output[4][4];
+    uint8_t AES128_attendu[4][4] = {{0x39, 0x02, 0xdc, 0x19}, {0x25, 0xdc, 0x11, 0x6a}, {0x84, 0x09, 0x85, 0x0b}, {0x1d, 0xfb, 0x97, 0x32}};
+    print_state(AES128_attendu);
+
+
+    
+    cipher(input, key, AES128_output);
+    
+    printf("Test cipher AES-128 : ");
+
+    if (compare(AES128_output, AES128_attendu)) {
+        printf("True\n\n");
+    } else {
+        printf("False\n\n");
+    }
+
+    print_state(AES128_output);
+
 
     return 0;
 }
